09380be051912cad347b59520a8028241d27ecb6
haodev007 <haodev007@gmail.com>
2023-04-13 20:43:47 -0400

    CRUD and well-documented API

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,34 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ArticlesService } from './articles.service';
-import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
 
-@Controller('articles')
+import { ArticlesService } from "./articles.service";
+import { CreateArticleDto } from "./dto/create-article.dto";
+import { UpdateArticleDto } from "./dto/update-article.dto";
+import { ArticleEntity } from "./entities/article.entity";
+
+@Controller("articles")
+@ApiTags("articles")
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: ArticleEntity })
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articlesService.create(createArticleDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
   findAll() {
     return this.articlesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get("drafts")
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
+  findDrafts() {
+    return this.articlesService.findDrafts();
+  }
+
+  @Get(":id")
+  @ApiOkResponse({ type: ArticleEntity })
+  findOne(@Param("id") id: string) {
     return this.articlesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
+  @Patch(":id")
+  @ApiOkResponse({ type: ArticleEntity })
+  update(@Param("id") id: string, @Body() updateArticleDto: UpdateArticleDto) {
     return this.articlesService.update(+id, updateArticleDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  @ApiOkResponse({ type: ArticleEntity })
+  remove(@Param("id") id: string) {
     return this.articlesService.remove(+id);
   }
 }
